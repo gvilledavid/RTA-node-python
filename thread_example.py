c1bdf92c2b94ca270ae8f5c10c1512511b57cdbe
srazojr <srazojr@gmail.com>
2023-05-22 10:17:24 -0400

    threading demo

--- thread_example.py
+++ thread_example.py
@@ -3,26 +3,39 @@ from threading import Thread
 from time import sleep
 class leaf_runner:
     def __init__(self, sleep_time,name):
+        print("parent constructor")
         self.command_queue=Queue()
         self.name=name
         self.output_queue=Queue()
-        self.runner=Thread(target=self.run(),args=())
         self.sleep_time=sleep_time
         self.data=0
+        self.runner=Thread(target=self.run,args=())
+        print("ending parent constructor")
     def run(self):
         while True:
             print(f"Thread {self.name} running.")
-            self.output_queue.put(data)
-            data+=1
+            self.output_queue.put(self.data)
+            self.data+=1
             while(self.command_queue.qsize()):
                 print(f"Recieved {self.command_queue.get()}")
             print(f"Thread {self.name} sleeping.")
             sleep(self.sleep_time)
-
+class test(leaf_runner):
+    def __init__(self):
+        
+        print("print inheritor constructor")
 if __name__=="__main__":
+    test()
+    while 1:
+        pass
     leaf1=leaf_runner(2,"fast")
     leaf2=leaf_runner(10,"slow")
-    leaf1.runner.start()
     leaf2.runner.start()
-    while input().lower()!="exit\n":
+    leaf1.runner.start()
+    data=0
+    while True:
+        leaf1.command_queue.put(data)
+        leaf2.command_queue.put(data)
+        data+=1
+        sleep(5)
         pass
