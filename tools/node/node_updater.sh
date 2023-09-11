91c8c154755129c1828a0f3d9b592f32e63170e8
srazojr <srazojr@ufl.edu>
2023-09-11 16:53:23 -0400

    update script

--- tools/node/node_updater.sh
+++ tools/node/node_updater.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+echo "Killing old process..."
 kill $(cat /usr/src/RTA/node/node_pid.file)
 sleep 3
+echo "Deleting old files."
 cd /usr/src/RTA/
 rm -rf RTA-node-python
+echo "Cloning new repo."
 git clone --branch test "$(cat /usr/local/share/.secrets/git.store)"/Convergent-Engineering/RTA-node-python.git
+echo "Moving /usr/src/RTA/node scripts"
 mv -f /usr/src/RTA/RTA-node-python/tools/node/* /usr/src/RTA/node/.
 chmod +777 /usr/src/RTA/node/node_updater.sh
 chmod +777 /usr/src/RTA/node/start.sh
 chmod +777 /usr/src/RTA/node/pulse.sh
 chmod +777 /usr/src/RTA/node/node_start.sh
 chmod +777 /usr/src/RTA/node/gpiostat.sh
+echo "Starting NodeManager..."
 ./node/node_start.sh
+echo "Done"
+echo "PID of NodeManager: $(cat node_pid.file)"
