abdc7532bce89122cc875909070db6a9691db368
srazojr <srazojr@ufl.edu>
2023-09-29 12:15:51 -0400

    update script improvements

--- tools/node/node_updater.sh
+++ tools/node/node_updater.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
+if whoami |grep root
+then
+    echo "Elevated permissions"
+else
+    echo "Run as root, exiting"
+    exit
+fi
 echo "Killing old process..."
+kill -TERM $(cat /usr/src/RTA/commands_driver/pid.file)
+echo "commands driver down"
 kill -TERM $(cat /usr/src/RTA/node/node_pid.file)
+echo "Node down"
+kill -TERM $(cat /usr/src/RTA/uart_manager/pid.file)
+echo "uart manager down"
+
 sleep 3
 echo "Deleting old files."
 cd /usr/src/RTA/
 rm -rf RTA-node-python
+rm -rf uart_driver
+rm -rf commands_driver
+
 echo "Cloning new repo."
-git clone --branch test "$(cat /usr/local/share/.secrets/git.store)"/Convergent-Engineering/RTA-node-python.git
+if [ $# -eq 0 ]
+then
+        BRANCH="test"
+else
+        BRANCH=$1
+fi
+echo "Using github branch $BRANCH"
+
+git clone --branch $BRANCH "$(cat /usr/local/share/.secrets/git.store)"/Convergent-Engineering/RTA-node-python.git
 echo "Moving /usr/src/RTA/node scripts"
 mv -f /usr/src/RTA/RTA-node-python/tools/node/* /usr/src/RTA/node/.
 chmod +777 /usr/src/RTA/node/node_updater.sh
@@ -14,11 +38,78 @@ chmod +777 /usr/src/RTA/node/start.sh
 chmod +777 /usr/src/RTA/node/pulse.sh
 chmod +777 /usr/src/RTA/node/node_start.sh
 chmod +777 /usr/src/RTA/node/gpiostat.sh
+
+echo "Moving startup drivers"
+#mkdir -p /usr/src/RTA/uart_driver
+#mkdir -p /usr/src/RTA/commands_driver
+cp -r /usr/src/RTA/RTA-node-python/startup_driver/commands_driver  /usr/src/RTA/commands_driver
+cp -r /usr/src/RTA/RTA-node-python/startup_driver/uart_driver  /usr/src/RTA/uart_driver
+#restart the startup drivers as root
+chmod +777 /usr/src/RTA//uart_driver/setup.sh
+chmod +777 /usr/src/RTA/commands_driver/setup.sh
+echo "Starting startup_drivers"
+sudo /usr/src/RTA/commands_driver/setup.sh
+sudo /usr/src/RTA/uart_driver/setup.sh
+
+sleep 10
 echo "Starting NodeManager..."
 #/bin/bash /usr/src/RTA/node/node_start.sh
 sleep 3
 cd /usr/src/RTA/RTA-node-python/node_manager/
 nohup python3 NodeManager.py >/dev/null 2>&1 &
 echo $!>/usr/src/RTA/node/node_pid.file
+
+#verify crontab
+cd /usr/src/RTA/node/
+sudo crontab -u root  -l > root_crontab.txt
+sudo crontab -u ubuntu  -l > ubuntu_crontab.txt
+
+if grep "/usr/src/RTA/uart_driver/setup.sh" root_crontab.txt
+then
+    echo "uart_driver exists in root crontab"
+else
+    echo "adding uart_driver to root crontab"
+    echo "@reboot /usr/src/RTA/uart_driver/setup.sh">>root_crontab.txt
+fi
+if grep "/usr/src/RTA/commands_driver/setup.sh" root_crontab.txt
+then
+    echo "commands_driver exists in root crontab"
+else
+    echo "adding commands_driver to root crontab"
+    echo "@reboot /usr/src/RTA/commands_driver/setup.sh">>root_crontab.txt
+fi
+#unnatended updates should also be in root crontab I guess
+if grep "/usr/src/RTA/RTA-node-python/tools/miamihosts.py" ubuntu_crontab.txt
+then
+    echo "miamihosts.py exists in ubuntu crontab"
+else
+    echo "adding miamihosts.py to ubuntu crontab"
+    echo "@reboot /usr/src/RTA/RTA-node-python/tools/miamihosts.py">>ubuntu_crontab.txt
+fi
+if grep "/usr/src/RTA/node/start.sh" ubuntu_crontab.txt
+then
+    echo "test pulse exists in ubuntu crontab"
+else
+    echo "adding test pulse to ubuntu crontab"
+    echo "@reboot /usr/src/RTA/node/start.sh">>ubuntu_crontab.txt
+fi
+if grep "/usr/src/RTA/node/node_start.sh" ubuntu_crontab.txt
+then
+    echo "node manager exists in ubuntu crontab"
+else
+    echo "adding node manager to ubuntu crontab"
+    echo "@reboot /usr/src/RTA/node/node_start.sh">>ubuntu_crontab.txt
+fi
+sudo crontab -u root  root_crontab.txt
+#@reboot /usr/src/RTA/uart_driver/setup.sh
+#@reboot /usr/src/RTA/commands_driver/setup.sh
+sudo crontab -u ubuntu   ubuntu_crontab.txt
+#@reboot /usr/src/RTA/RTA-node-python/tools/miamihosts.py
+#@reboot /usr/src/RTA/node/start.sh
+sudo rm root_crontab.txt
+sudo rm ubuntu_crontab.txt
+ 
 echo "Done"
 echo "PID of NodeManager: $(cat /usr/src/RTA/node/node_pid.file)"
+echo "PID of Uart Driver: $(cat /usr/src/RTA/uart_manager/pid.file)"
+echo "PID of Commands Driver: $(cat /usr/src/RTA/commands_driver/pid.file)"
